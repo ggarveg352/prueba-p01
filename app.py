780a0ac25b000f94e548defcfcc3f0d947aefd68
Gonzalo <ggarveg352@g.educaand.es>
2026-01-19 20:47:19 +0100

    segunda prueba del Action: no debe dejar merge

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
- 
 def suma(a: int, b: int) -> int:
-        """ docstring function
-        # debe dar errores en linter
-        """
-        x = "no se usa"
-        return a+ b
+    """ docstring function
+    # debe dar errores en linter
+    """
+    x = "no se usa"
+    _ = x  # uso dummy, pero linter lo marca unused
+    return a + b
